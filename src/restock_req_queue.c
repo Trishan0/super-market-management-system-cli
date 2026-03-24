46d91186fcef1f0b787aa6b79e015118db131024
Dinuka <dinuka6685@gmail.com>
2026-03-24 16:57:13 +0530

    Implement enqueue function for restock requests and queue management checks

--- src/restock_req_queue.c
+++ src/restock_req_queue.c
@@ -13,3 +13,30 @@ void initQueue(void) {
     restockQueue.items[1] = (RestockRequest){102, "Milk Powder", 10};
     restockQueue.items[2] = (RestockRequest){104, "Tea Pack", 15};
 }
+
+static int isQueueEmpty(void) {
+    return restockQueue.front > restockQueue.rear;
+}
+
+static int isQueueFull(void) {
+    return restockQueue.rear == MAX_QUEUE - 1;
+}
+
+void enqueueRestockRequest(void) {
+    if (isQueueFull()) {
+        printf("Restock request queue is full.\n");
+        return;
+    }
+
+    RestockRequest r;
+    printf("Enter product id: ");
+    scanf("%d", &r.productId);
+    printf("Enter product name: ");
+    scanf(" %49[^\n]", r.productName);
+    printf("Enter requested quantity: ");
+    scanf("%d", &r.requestedQty);
+
+    restockQueue.rear++;
+    restockQueue.items[restockQueue.rear] = r;
+    printf("Restock request added successfully.\n");
+}

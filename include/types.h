bbf02cd0e57cb5c6e0de501b2af61f07749c43a8
Kaveesha Nethmal <nethmalkaveesha456@gmail.com>
2026-03-24 19:04:40 +0530

    Refractor : Update types.h adding Declarations

--- include/types.h
+++ include/types.h
@@ -1,9 +1,47 @@
 #ifndef TYPES_H
 #define TYPES_H
-#define NAME_LEN 50
 
+#define MAX_PRODUCTS 200
+#define MAX_SALES 200
+#define MAX_QUEUE 100
 #define MAX_STACK 100
+#define NAME_LEN 50
 #define DESC_LEN 100
+
+typedef struct {
+    int id;
+    char name[NAME_LEN];
+    float price;
+    int quantity;
+} Product;
+
+typedef struct Supplier {
+    int id;
+    char name[NAME_LEN];
+    char phone[20];
+    struct Supplier* next;
+} Supplier;
+
+typedef struct Purchase {
+    int recordId;
+    int customerId;
+    char customerName[NAME_LEN];
+    float amount;
+    struct Purchase* prev;
+    struct Purchase* next;
+} Purchase;
+
+typedef struct {
+    int productId;
+    char productName[NAME_LEN];
+    int requestedQty;
+} RestockRequest;
+
+typedef struct {
+    RestockRequest items[MAX_QUEUE];
+    int front;
+    int rear;
+} RestockQueue;
 
 typedef struct {
     int productId;
@@ -23,11 +61,9 @@ typedef struct Promo {
     struct Promo* next;
 } Promo;
 
-typedef struct Purchase {
-    int recordId;
-    int customerId;
-    char customerName[NAME_LEN];
+typedef struct {
+    int day;
     float amount;
-    struct Purchase* prev;
-    struct Purchase* next;
-} Purchase;
+} SaleRecord;
+
+#endif

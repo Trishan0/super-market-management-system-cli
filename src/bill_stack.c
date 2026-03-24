6b0e9afb32922fe9ff7f8192eb16de52055f4662
Ravindu200324511398 <ravindumcg2003@gmail.com>
2026-03-24 18:10:28 +0530

    Implement pushScannedItem function to add items to the bill stack

--- src/bill_stack.c
+++ src/bill_stack.c
@@ -12,3 +12,31 @@ void initBillStack(void) {
     bill.items[2] = (BillItem){104, "Tea Pack", 480.00f, 1};
 }
 
+static int isStackEmpty(void) {
+    return bill.top == -1;
+}
+
+static int isStackFull(void) {
+    return bill.top == MAX_STACK - 1;
+}
+
+void pushScannedItem(void) {
+    if (isStackFull()) {
+        printf("Bill stack is full.\n");
+        return;
+    }
+
+    BillItem item;
+    printf("Enter product id: ");
+    scanf("%d", &item.productId);
+    printf("Enter product name: ");
+    scanf(" %49[^\n]", item.productName);
+    printf("Enter price: ");
+    scanf("%f", &item.price);
+    printf("Enter quantity: ");
+    scanf("%d", &item.qty);
+
+    bill.top++;
+    bill.items[bill.top] = item;
+    printf("Item pushed to current bill.\n");
+}

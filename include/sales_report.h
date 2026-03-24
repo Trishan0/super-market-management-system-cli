881c68efbc7898614c7603fb918c5f3b5da4ac91
Nesandu Hesitha <nesanduhm@gmail.com>
2026-03-24 17:28:12 +0530

    Add sales report header file with function declarations

---
+++ include/sales_report.h
@@ -0,0 +1,16 @@
+#ifndef SALES_REPORT_H
+#define SALES_REPORT_H
+
+void salesReportMenu(void);
+void initSales(void);
+void addSalesRecord(void);
+void searchSalesRecord(void);
+void updateSalesRecord(void);
+void deleteSalesRecord(void);
+void displaySalesRecords(void);
+void bubbleSortSalesByAmount(void);
+void totalDailySales(void);
+void averageSales(void);
+void highestSaleDay(void);
+
+#endif
